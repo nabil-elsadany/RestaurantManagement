3b21c8259968106058f6eba9d8a0ebda898a54e1
Mohamed7amada <m7amd7amada@gmail.com>
2025-08-19 15:49:50 +0300

    refactor(category): use default values for non nullable types

--- RestaurantManagement/Models/Category.cs
+++ RestaurantManagement/Models/Category.cs
@@ -3,11 +3,11 @@
     public class Category
     {
         public int Id { get; set; }
-        public string Name { get; set; } 
-        public string Description { get; set; }
+        public string Name { get; set; } = default!;
+        public string Description { get; set; }= default!;
 
 
-        public ICollection<Dish> dishes { get; set; }
-        public ICollection<User> Users { get; set; }
+        public ICollection<Dish> dishes { get; set; }= default!;
+        public ICollection<User> Users { get; set; }= default!;
     }
 }
